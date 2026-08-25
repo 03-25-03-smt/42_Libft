ea6d4c4a76a4a28fc9bf5f0c93c509d402c5c979
03-25-03-smt <vlad40303@gmail.com>
2026-08-25 22:14:21 +0200

    Create ft_strlen.c

---
+++ ft_strlen.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+    const char  *str;
+    size_t  i;
+
+    str = s;
+    i = 0;
+    while (str[i] != '\0')
+      {
+        i++;
+      }
+    return (i);
+}
