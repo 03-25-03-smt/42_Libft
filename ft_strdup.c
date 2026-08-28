063b0909d780996d224d2a9b4e7ea56682649375
03-25-03-smt <vlad40303@gmail.com>
2026-08-28 10:26:24 +0200

    Create ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+  int  len;
+  char  *c;
+
+  len = ft_strlen(s);
+  c = malloc(len + 1);
+  if (!c)
+    return (NULL);
+  ft_memcpy()
+
+  
+  
