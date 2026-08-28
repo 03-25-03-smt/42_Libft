7f01461380ee4a71047b74756a950a7d7487ba1c
03-25-03-smt <vlad40303@gmail.com>
2026-08-28 11:24:34 +0200

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -7,7 +7,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
   i = 0;
   if (dstsize > 0)
 	{
-		while (src[i] && i < size - 1)
+		while (src[i] && i < dstsize - 1)
 		{
 			dst[i] = src[i];
 			i++;
