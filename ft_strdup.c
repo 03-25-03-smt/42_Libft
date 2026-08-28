26512a7c8b9575a6ebe7e8860bed5e73bcc23467
03-25-03-smt <vlad40303@gmail.com>
2026-08-28 10:40:58 +0200

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -2,14 +2,17 @@
 
 char	*ft_strdup(const char *s)
 {
-  int  len;
+  size_t  len;
   char  *c;
 
   len = ft_strlen(s);
   c = malloc(len + 1);
   if (!c)
     return (NULL);
-  ft_memcpy()
+  ft_memcpy(c,s,len);
+  c[len] = '\0';
+  return (c);
+}
 
   
   
