38b29529e291874203dbe6c495b3266e5b50e8db
03-25-03-smt <vlad40303@gmail.com>
2026-09-03 13:03:26 +0200

    Update ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -5,16 +5,16 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	t_list	*next;
 	t_list	*current;
 
-	if (lst != NULL && del != NULL)
+	if (lst == NULL)
+		return ;
+	current = *lst;
+	while (current != NULL)
 	{
-		current = *lst;
-		while (current != NULL)
-		{
-			next = current->next;
+		next = current->next;
+		if (del)
 			del(current->content);
-			free(current);
-			current = next;
-		}
-		*lst = NULL;
+		free(current);
+		current = next;
 	}
+	*lst = NULL;
 }
