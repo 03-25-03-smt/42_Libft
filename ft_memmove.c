54845e7668449df93769313a5a298c5fe3121b34
03-25-03-smt <vlad40303@gmail.com>
2026-09-03 12:50:40 +0200

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,34 +2,24 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-  unsigned char *dest_ptr;
-  const unsigned char *src_ptr;
+	char		*pdest;
+	const char	*psrc;
 
-  if (!dest && !src)
+	if (!dest && !src)
 		return (NULL);
-  dest_ptr = dest;
-  src_ptr = src;
-  if (dest_ptr < src_ptr)
-  {
-    while(n > 0)
-      {
-        *dest_ptr = *src_ptr;
-        dest_ptr++;
-        src_ptr++;
-        n--;
-      }
-  }
-  else
-  {
-      src_ptr += n;
-      dest_ptr += n;
-      while(n > 0)
-      {
-        src_ptr--;
-        dest_ptr--;
-        *dest_ptr = *src_ptr;
-        n--;
-      }
-  }
-  return (dest);
+	pdest = dest;
+	psrc = src;
+	if (pdest < psrc)
+	{
+		while (n--)
+			*pdest++ = *psrc++;
+	}
+	else
+	{
+		pdest += n;
+		psrc += n;
+		while (n--)
+			*(--pdest) = *(--psrc);
+	}
+	return (dest);
 }
