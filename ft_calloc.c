44f06255f90a34bbca80b71b8aa7e644b6b2408e
03-25-03-smt <vlad40303@gmail.com>
2026-09-03 13:01:35 +0200

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,14 +1,16 @@
 #include "libft.h"
 
-void  *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-  size_t  sum;
-  size_t  *p;
+	size_t	sum;
+	void	*p;
 
-  sum = nmemb * size;
-  p = malloc(sum);
-  if (!p)
-    return (NULL);
-ft_memset(p,0,sum);
-return (p);
+	if (nmemb != 0 && size > (size_t)-1 / nmemb)
+		return (NULL);
+	sum = nmemb * size;
+	p = malloc(sum);
+	if (!p)
+		return (NULL);
+	ft_memset(p, 0, sum);
+	return (p);
 }
