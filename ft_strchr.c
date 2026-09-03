0266aceb93e6f3c1603d8257234402544e79d94f
03-25-03-smt <vlad40303@gmail.com>
2026-09-03 12:07:17 +0200

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -5,7 +5,7 @@ char	*ft_strchr(const char *s, int c)
   while (1)
     {
       if (c == *s)
-        return (s);
+        return ((char *)s);
       if (*s == '\0')
 		    return (NULL);
       s++;
